ae2fbf0d7cd39c58cc9a175f1f196415eb3d832b
valeria caramellino <valeriacaramellino@gmail.com>
2023-07-12 10:32:42 +0200

    genera type_id nel seeder project

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -1,8 +1,8 @@
 <?php
 
 namespace Database\Seeders;
-
 use App\Models\Project;
+use App\Models\Type;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Faker\Generator as Faker;
@@ -16,11 +16,14 @@ class ProjectSeeder extends Seeder
      */
     public function run(Faker $faker)
     {
+        $types = Type::all();
+
         for ($i=0; $i < 10 ;$i ++ ){
             $project = new Project();
             $project->title = $faker->words(2, true);
             $project->content = $faker->text(200);
             $project->image= $faker->imageUrl(800 , 600, 'animals', true);
+            $project->type_id = rand(1, count($types));
             $project->save();
         }
     }
